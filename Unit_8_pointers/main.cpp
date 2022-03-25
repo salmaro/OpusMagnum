754ad6910ea5049d8d1620caf07f4e29f5eb45a8
Marcin <salmaro@op.pl>
2022-03-25 20:38:32 +0100

    Initial Commit

---
+++ Unit_8_pointers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unit_8_pointers
+//
+//  Created by Marcin Salamon on 25/03/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
